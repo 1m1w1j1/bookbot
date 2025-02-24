332f53bff936b7e3af533f21d856b240e28d6e27
1m1w1j1 <1mark1w1jones1@gmail.com>
2025-02-24 18:14:30 -0500

    Added more to stats.py for text manipulation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from stats import get_words_number
-
+from stats import character_count
 
 
 def get_book_text(path_to_file):
@@ -12,5 +12,6 @@ def main():
     book = get_book_text('books/frankenstein.txt')
     word_count = get_words_number(book)
     print(f"{word_count} words found in the document")
-
+    characters = character_count(book)
+    print(characters)
 main()
--- stats.py
+++ stats.py
@@ -1,7 +1,16 @@
 def get_words_number(file_contents):
-    wourd_count = 0
+    word_count = 0
     word_list = []
     word_list = file_contents.split()
     word_count = len(word_list)
     return word_count
 
+def character_count(file_contents):
+    alphabet = {}
+    lower_string = file_contents.lower()
+    for char in lower_string:
+        if char in alphabet:
+            alphabet[char] += 1
+        else:
+            alphabet[char] = 1
+    return alphabet
